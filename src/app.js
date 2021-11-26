7118caf3aa31094208bf8a93123427b5f7525977
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2021-11-26 00:46:10 -0300

    correção de erros no app e adição da controller

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const cors = require('cors')
 
 const port = 3000
 
+//importando controllers
+
+//importanto o bancdo de dados
+const bd = require('./database/sqlite-db')
+
 //middlewares
 app.use(express.json())
 app.use((req, res, next)=>
--- src/model/Student.js
+++ src/model/Student.js
@@ -1,10 +1,11 @@
 class Student
 {
-  constructor(name, birthDate, CPF, career)
+  constructor(name, birthDate, CPF, email, career)
   {
     this.name = name
     this.birthDate = birthDate
     this.CPF = CPF
+    this.email = email
     this.career = career
     this.registrationDate = Date.now()
   }
