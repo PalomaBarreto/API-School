e2b3974f83c9552bda055c89bf64eae852dc4d46
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2021-11-27 22:19:53 -0300

    rota de get único funcionando

--- src/controller/students-controller.js
+++ src/controller/students-controller.js
@@ -23,16 +23,41 @@ const student = (app,db)=>
   {
     try
     {
-      const body = re.body
-      const newStudent = new Student(body.name)
+      const body = req.body
+      const newStudent = new Student(body.name, body.birthData, body.CPF, body.email, body.career)
+
+      //lógica de inserção
+      const resp = await newStudentDAO.insertStudent(newStudent)
+      res.json(resp)
     }
     catch (error)
     {
-      
+      res.json
+      ({
+        "msg": error.message,
+        "error": true
+      })
     }
   })
 
-  
+  //get único
+  app.get('/students/:id', async (req, res)=>
+  {
+    const id = req.params.id
+
+    //lógica de busca
+    try
+    {
+      const resp = await newStudentDAO.getById(id)
+      res.json(resp)
+    }
+    catch (error)
+    {
+      res.status(404).json(error)
+    }
+
+  })
+
 }
 
 module.exports = student
