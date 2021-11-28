faf788896654d7f42c7e8fd24e9e382a8d74a5e2
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2021-11-28 00:15:55 -0300

    finalizando CRUD

--- src/DAO/StudentDAO.js
+++ src/DAO/StudentDAO.js
@@ -85,6 +85,79 @@ class StudentDAO
       })
     })
   }
+
+  async deleteStudent(id)
+  {
+    try
+    {
+      const student = await this.getById(id)
+
+      if (student.req.length)
+      {
+        const del = `DELETE FROM STUDENTS WHERE ID = ?`
+
+        return new Promise((resolve, reject)=>
+        {
+          this.db.run(del, id, (error)=>
+          {
+            if (error)
+            {
+              reject(error.message)  
+            }
+            else
+            {
+              resolve
+              ({
+                "msg": `O aluno com id ${id} foi deletado`,
+                "error": false
+              })
+            }
+          })
+        })
+      }
+      else
+      {
+        throw new Error(`Aluno com id ${id} não existe`)
+      }
+    }
+    catch (error)
+    {
+      throw new Error(error.message)
+    }
+  }
+
+  async updateStudent(id, newStudent)
+  {
+    try
+    {
+      const up = `UPDATE STUDENTS SET NAME = ?, BIRTHDATE = ?, CPF = ?, EMAIL = ?, CAREER = ?
+      WHERE ID = ? `
+
+      return new Promise((resolve, reject)=>
+      {
+        this.db.run(up, [newStudent.name, newStudent.birthDate, newStudent.CPF, newStudent.email, newStudent.career, id], (error)=>
+        {
+          if (condition)
+          {
+            reject(error)
+          }
+          else
+          {
+            resolve
+            ({
+              "msg": `O aluno com id ${id} foi atualizado`,
+              "student": newStudent,
+              "error": false
+            })
+          }
+        })
+      })
+    }
+    catch (error)
+    {
+      throw new Error(error.message)
+    }
+  }
 }
 
 module.exports = StudentDAO
--- src/controller/students-controller.js
+++ src/controller/students-controller.js
@@ -24,7 +24,7 @@ const student = (app,db)=>
     try
     {
       const body = req.body
-      const newStudent = new Student(body.name, body.birthData, body.CPF, body.email, body.career)
+      const newStudent = new Student(body.name, body.birthDate, body.CPF, body.email, body.career)
 
       //lógica de inserção
       const resp = await newStudentDAO.insertStudent(newStudent)
@@ -56,6 +56,69 @@ const student = (app,db)=>
       res.status(404).json(error)
     }
 
+  })
+
+  app.delete('/students/:id', async (req, res)=>
+  {
+    const id = parseInt(req.params.id)
+
+    try
+    {
+      const resp = await newStudentDAO.deleteStudent(id)
+      res.json(resp)
+    }
+    catch (error)
+    {
+      res.status(404).json
+      ({
+        "msg": error.message,
+        "error": true
+      })
+    }
+  })
+
+  app.put('/students/:id', async (req, res)=>
+  {
+    const id = req.params.id
+    const body = req.body
+
+    //lógica para atualizar
+    try
+    {
+      const respGet = await newStudentDAO.getById(id)
+      const beforeUpdate = respGet.req[0]
+
+      if (beforeUpdate)
+      {
+        const updatedStudent = new Student
+        (
+          body.name || beforeUpdate.NAME,
+          body.birthDate || beforeUpdate.BIRTHDATE,
+          body.CPF || beforeUpdate.CPF,
+          body.email || beforeUpdate.EMAIL,
+          body.career || beforeUpdate.CAREER
+        )
+
+        const resp = await newStudentDAO.updateStudent(id, updatedStudent)
+        res.json(resp)
+      }
+      else
+      {
+        res.json
+        ({
+          "msg": `Aluno com id ${id} não existe`,
+          "error": true
+        })
+      }
+    }
+    catch (error)
+    {
+      res.json
+      ({
+        "msg": error.message,
+        "error": true
+      })
+    }
   })
 
 }
