3b93d25ba8f51747b8a289ef0131d0e1c1ec409d
Paloma Barreto de Carvalho <86432423+PalomaBarreto@users.noreply.github.com>
2021-11-28 12:56:20 -0300

    correção no argumento 'data de registro'

--- src/model/Student.js
+++ src/model/Student.js
@@ -1,13 +1,13 @@
 class Student
 {
-  constructor(name, birthDate, CPF, email, career)
+  constructor(name, birthDate, CPF, email, career, registrationDate)
   {
     this.name = name
     this.birthDate = birthDate
     this.CPF = CPF
     this.email = email
     this.career = career
-    this.registrationDate = Date.now()
+    this.registrationDate = registrationDate
   }
 }
 
